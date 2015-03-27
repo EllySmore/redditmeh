af88e689f6831d6abacbf42767691de7d5ee7d15
Eileen Yau <yau.eln@gmail.com>
2015-03-27 00:04:42 -0400

    Created constant path. Added paginated endpoint.

--- app/src/main/java/ellysmore/redditmeh/api/services/RedditApiService.java
+++ app/src/main/java/ellysmore/redditmeh/api/services/RedditApiService.java
@@ -1,20 +1,29 @@
 package ellysmore.redditmeh.api.services;
 
-
+import ellysmore.redditmeh.api.ApiPath;
 import ellysmore.redditmeh.api.models.Listing.Listing;
+import retrofit.http.Field;
 import retrofit.http.GET;
 import retrofit.http.Path;
 import rx.Observable;
 
 public interface RedditApiService {
 
-    @GET("/r/{SUBREDDIT_NAME}/{LISTING_TYPE}/.json")
+    @GET(ApiPath.SUBREDDIT_LISTING)
     Observable<Listing> getSubRedditListing(
             @Path("SUBREDDIT_NAME") String subredditName,
             @Path("LISTING_TYPE") String listingType
     );
 
-    @GET("/{LISTING_TYPE}/.json")
+    @GET(ApiPath.SUBREDDIT_LISTING)
+    Observable<Listing> getNextSubredditListing(
+            @Path("SUBREDDIT_NAME") String subredditName,
+            @Path("LISTING_TYPE") String listingType,
+            @Field("after") String afterFullName,
+            @Field("limit") int limit
+    );
+
+    @GET(ApiPath.FRONT_PAGE_LISTING)
     Observable<Listing> getFrontPageListing(
             @Path("LISTING_TYPE") String listingType
     );
