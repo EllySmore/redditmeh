b63c65ed900d14dae239d33df90caf5aa5df42e8
Eileen Yau <yau.eln@gmail.com>
2015-03-27 17:06:46 -0400

    Removed annotation Field to Query. Added constant int for limit size. Added path to get nextfrontpage listings.

--- app/src/main/java/ellysmore/redditmeh/api/services/RedditApiService.java
+++ app/src/main/java/ellysmore/redditmeh/api/services/RedditApiService.java
@@ -2,9 +2,9 @@ package ellysmore.redditmeh.api.services;
 
 import ellysmore.redditmeh.api.ApiPath;
 import ellysmore.redditmeh.api.models.Listing.Listing;
-import retrofit.http.Field;
 import retrofit.http.GET;
 import retrofit.http.Path;
+import retrofit.http.Query;
 import rx.Observable;
 
 public interface RedditApiService {
@@ -19,12 +19,19 @@ public interface RedditApiService {
     Observable<Listing> getNextSubredditListing(
             @Path("SUBREDDIT_NAME") String subredditName,
             @Path("LISTING_TYPE") String listingType,
-            @Field("after") String afterFullName,
-            @Field("limit") int limit
+            @Query("after") String afterFullName,
+            @Query("limit") int limit
     );
 
     @GET(ApiPath.FRONT_PAGE_LISTING)
     Observable<Listing> getFrontPageListing(
             @Path("LISTING_TYPE") String listingType
+    );
+
+    @GET(ApiPath.FRONT_PAGE_LISTING)
+    Observable<Listing> getNextFrontPageListing(
+            @Path("LISTING_TYPE") String listingType,
+            @Query("after") String afterFullName,
+            @Query("limit") int limit
     );
 }
